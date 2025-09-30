ee95662f0749153f93915677466020e9e4421df8
monica-land <landaverde.martinez2006@gmail.com>
2025-09-30 08:05:24 -0600

    update:Se creo la vista de el formulario

---
+++ examen_metodologia_2_monica_landaverde/src/views/Examen2_Monica_Landaverde.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Formulario - Monica Landaverde</h1>
+  <div>
+    <form action="GET">
+      <label for="">Ingresa tu nombre de usuario</label>
+      <input type="text" placeholder="nombre de usuario" />
+    </form>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
